edfd08b3d3a7cd5fca99131497d686aacd3801d3
Nimisha601 <53156786+Nimisha601@users.noreply.github.com>
2025-09-29 18:03:45 +0530

    Add solution: PacificAtlanticWaterFlow

---
+++ Problems/PacificAtlanticWaterFlow.py
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=1785284543 lang=python3
+ *
+ * PacificAtlanticWaterFlow
+ * 
+ * Difficulty: Level
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        rows , cols =len(heights),len(heights[0])
+        pac,alt=set(),set()
+        def dfs(r,c,visit,prevheight):
+            if (r,c) in visit or r<0 or c<0 or r ==rows or c ==cols or heights[r][c]<prevheight:
+                return 
+            visit.add((r,c))
+            dfs(r+1,c,visit,heights[r][c]) 
+            dfs(r-1,c,visit,heights[r][c])
+            dfs(r,c+1,visit,heights[r][c])
+            dfs(r,c-1,visit,heights[r][c])   
+
+        for c in range(cols):
+            dfs(0,c,pac,heights[0][c])
+            dfs(rows-1,c,alt,heights[rows-1][c])
+        for r in range(rows):
+            dfs(r,0,pac,heights[r][0])
+            dfs(r,cols-1,alt,heights[r][cols-1])  
+        res=[]    
+        for r in range(rows):
+            for c in range(cols):
+                if(r,c) in pac and (r,c) in alt:
+                    res.append((r,c))
+        return res
+
+
+
+        
