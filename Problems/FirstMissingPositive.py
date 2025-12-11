3860cc4671858e8e6c5f179b4cda151d805dad74
Nimisha601 <53156786+Nimisha601@users.noreply.github.com>
2025-12-11 20:42:47 +0530

    Add solution: FirstMissingPositive

---
+++ Problems/FirstMissingPositive.py
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=1830162436 lang=python3
+ *
+ * FirstMissingPositive
+ * 
+ * Difficulty: Level
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            if nums[i]<0:
+               nums[i]=0
+        for i in range(len(nums)):
+            val = abs(nums[i])
+            if 1<=val<=len(nums):
+                if nums[val-1]>0:
+                    nums[val-1]*=-1
+                elif nums[val-1]==0:
+                    nums[val-1]=-1*(len(nums)+1)
+        for k in range(1,len(nums)+1):
+            if nums[k-1]>=0:
+                return k
+        return len(nums)+1                              
+        
