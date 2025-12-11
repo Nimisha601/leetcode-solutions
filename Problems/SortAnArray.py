34a7b38e21546d12eb08eb30f565c36f866290df
Nimisha601 <53156786+Nimisha601@users.noreply.github.com>
2025-12-11 20:42:44 +0530

    Add solution: SortAnArray

---
+++ Problems/SortAnArray.py
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=1822425637 lang=python3
+ *
+ * SortAnArray
+ * 
+ * Difficulty: Level
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums)<=1:
+            return nums
+        mid = len(nums)//2
+        left =self.sortArray(nums[:mid])
+        right = self.sortArray(nums[mid:])
+        return self.merge(left,right)
+
+    def merge(self,left,right):
+        ans=[]
+        i =0
+        j=0
+        while i<len(left) and j<len(right) :
+          if left[i]<right[j]:
+            ans.append(left[i])
+            i+=1
+          else:
+            ans.append(right[j])
+            j+=1
+        ans.extend(left[i:])
+        ans.extend(right[j:]) 
+        return ans           
+
+        
